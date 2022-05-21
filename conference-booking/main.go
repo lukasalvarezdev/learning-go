3b015edb79ac71ef9a222086cdd580818b902e53
Lukas Alvarez <53347228+lukasalvarezdev@users.noreply.github.com>
2022-05-21 11:08:22 -0500

    fix(conference): add validation for negative numbers

--- conference-booking/main.go
+++ conference-booking/main.go
@@ -10,24 +10,36 @@ func main() {
 	remainingTickets, bookings := getBookings(totalTickets)
 
 	fmt.Printf("%s sold %d tickets, %d tickets remaining\n", conferenceName, len(bookings), remainingTickets)
+	fmt.Println("Bookings:", bookings)
 }
 
 func getBookings(remainingTickets uint) (remaining uint, bookings []string) {
 	for remainingTickets > 0 {
-		firstName, tickets := getUserInfo()
+		firstName, tickets := getUserInfo(remainingTickets)
 
 		bookings = append(bookings, firstName)
-		remainingTickets -= tickets
+
+		if tickets < 0 {
+			fmt.Println("Invalid number of tickets, must be greater than 0")
+			continue
+		}
+
+		if uint(tickets) > remainingTickets {
+			fmt.Println("Invalid number of tickets, must be less than or equal to", remainingTickets)
+			continue
+		}
+
+		remainingTickets -= uint(tickets)
 	}
 
 	return remainingTickets, bookings
 }
 
-func getUserInfo() (firstName string, tickets uint) {
-	fmt.Print("Welcome to Luki's concert, please enter your first name: ")
+func getUserInfo(remainingTickets uint) (firstName string, tickets int) {
+	fmt.Printf("Welcome to Luki's concert, there are %v tickets remaining, please enter your first name: \n", remainingTickets)
 	fmt.Scanln(&firstName)
 
-	fmt.Print("How many tickets do you want? ")
+	fmt.Print("How many tickets do you want? \n")
 	fmt.Scanln(&tickets)
 
 	return firstName, tickets
